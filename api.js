f97767d04aa2c21611ad3e63bf00ac052e34e4de
Clara Morin <clara.morin@icloud.com>
2024-03-06 09:50:02 +0100

    FEAT : Add arrow direction

--- api.js
+++ api.js
@@ -36,6 +36,26 @@ function calculateBeaufort(windSpeed) {
     }
 }
 
+function degreesToCardinal(degrees) {
+    if (degrees >= 337.5 || degrees < 22.5) {
+        return "N";
+    } else if (degrees >= 22.5 && degrees < 67.5) {
+        return "NE";
+    } else if (degrees >= 67.5 && degrees < 112.5) {
+        return "E";
+    } else if (degrees >= 112.5 && degrees < 157.5) {
+        return "SE";
+    } else if (degrees >= 157.5 && degrees < 202.5) {
+        return "S";
+    } else if (degrees >= 202.5 && degrees < 247.5) {
+        return "SW";
+    } else if (degrees >= 247.5 && degrees < 292.5) {
+        return "W";
+    } else {
+        return "NW";
+    }
+}
+
 var httpServer = http.createServer(function(request, response) {
     // Récupérer le chemin de l'URL
     const requestUrl = url.parse(request.url, true);
